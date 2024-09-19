f7365d5f7dace70460e55cbe7f904018c8f64758
rektdeckard <friedtm@gmail.com>
2024-09-18 22:33:28 -0600

    feat(agent): set up agent dispatch

---
+++ agent.py
@@ -0,0 +1,81 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    JobRequest,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, silero
+
+
+load_dotenv(dotenv_path=".env.local")
+sandbox = os.getenv("LIVEKIT_SANDBOX_ID")
+logger = logging.getLogger("voice-assistant")
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+            "You were created as a demo to showcase the capabilities of LiveKit's agents framework, "
+            "as well as the ease of development of realtime AI prototypes. You are currently running in a "
+            "LiveKit Sandbox, which is an environment that allows developers to instantly deploy prototypes "
+            "of their realtime AI applications to share with others."
+        ),
+    )
+
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+
+    # This project is configured to use OpenAI STT, LLM, and TTS plugins
+    # Other great providers exist like Cartesia and ElevenLabs
+    # Learn more and pick the best one for your app:
+    # https://docs.livekit.io/agents/plugins
+    assistant = VoiceAssistant(
+        vad=ctx.proc.userdata["vad"],
+        stt=openai.STT(model="whisper-1"),
+        llm=openai.LLM(),
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx,
+    )
+
+    assistant.start(ctx.room, participant)
+
+    # The agent should be polite and greet the user when it joins :)
+    await assistant.say("Hey, how can I help you today?", allow_interruptions=True)
+
+
+async def dispatch(ctx: JobRequest):
+    if sandbox is not None:
+        print(ctx.room.name)
+        if ctx.room.name.startswith("stateless"):
+            return await ctx.accept()
+        return await ctx.reject()
+    return await ctx.accept()
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm,
+            request_fnc=dispatch,
+        ),
+    )
