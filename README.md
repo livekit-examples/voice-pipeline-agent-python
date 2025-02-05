b4f928c80ef73dabbc9a7497f4c375ad4f0242e6
David Zhao <dz@livekit.io>
2025-02-04 16:58:28 -0800

    enable turn-detector by default
    also switching to Cartesia TTS for improved latency

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ cd voice-pipeline-agent-python
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+python3 agent.py download-files
 ```
 
 Set up the environment by copying `.env.example` to `.env.local` and filling in the required values:
@@ -33,6 +34,7 @@ Set up the environment by copying `.env.example` to `.env.local` and filling in 
 - `LIVEKIT_API_KEY`
 - `LIVEKIT_API_SECRET`
 - `OPENAI_API_KEY`
+- `CARTESIA_API_KEY`
 - `DEEPGRAM_API_KEY`
 
 You can also do this automatically using the LiveKit CLI:
--- agent.py
+++ agent.py
@@ -8,9 +8,10 @@ from livekit.agents import (
     WorkerOptions,
     cli,
     llm,
+    metrics,
 )
 from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.plugins import openai, deepgram, silero
+from livekit.plugins import cartesia, openai, deepgram, silero, turn_detector
 
 
 load_dotenv(dotenv_path=".env.local")
@@ -38,17 +39,28 @@ async def entrypoint(ctx: JobContext):
     participant = await ctx.wait_for_participant()
     logger.info(f"starting voice assistant for participant {participant.identity}")
 
-    # This project is configured to use Deepgram STT, OpenAI LLM and TTS plugins
-    # Other great providers exist like Cartesia and ElevenLabs
+    # This project is configured to use Deepgram STT, OpenAI LLM and Cartesia TTS plugins
+    # Other great providers exist like Cerebras, ElevenLabs, Groq, Play.ht, Rime, and more
     # Learn more and pick the best one for your app:
     # https://docs.livekit.io/agents/plugins
     agent = VoicePipelineAgent(
         vad=ctx.proc.userdata["vad"],
         stt=deepgram.STT(),
         llm=openai.LLM(model="gpt-4o-mini"),
-        tts=openai.TTS(),
+        tts=cartesia.TTS(),
+        turn_detector=turn_detector.EOUModel(),
+        # minimum delay for endpointing, used when turn detector believes the user is done with their turn
+        min_endpointing_delay=0.5,
+        # maximum delay for endpointing, used when turn detector does not believe the user is done with their turn
+        max_endpointing_delay=5.0,
         chat_ctx=initial_ctx,
     )
+
+    usage_collector = metrics.UsageCollector()
+    @agent.on("metrics_collected")
+    def on_metrics_collected(agent_metrics: metrics.AgentMetrics):
+        metrics.log_metrics(agent_metrics)
+        usage_collector.collect(agent_metrics)
 
     agent.start(ctx.room, participant)
 
