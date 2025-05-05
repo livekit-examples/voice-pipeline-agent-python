e208872811af6028b34e340631dbeaf2531717f8
Michael Solati 🥑 <mkslt04@gmail.com>
2025-05-05 08:31:02 -0700

    chore: migrate to v1 of agents library (#32)
    * chore: migrate to v1 of agents library

--- agent.py
+++ agent.py
@@ -2,27 +2,52 @@ import logging
 
 from dotenv import load_dotenv
 from livekit.agents import (
+    Agent,
+    AgentSession,
     AutoSubscribe,
     JobContext,
     JobProcess,
     WorkerOptions,
     cli,
-    llm,
     metrics,
+    RoomInputOptions,
 )
-from livekit.agents.pipeline import VoicePipelineAgent
 from livekit.plugins import (
     cartesia,
     openai,
     deepgram,
     noise_cancellation,
     silero,
-    turn_detector,
 )
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 
 load_dotenv(dotenv_path=".env.local")
 logger = logging.getLogger("voice-agent")
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        # This project is configured to use Deepgram STT, OpenAI LLM and Cartesia TTS plugins
+        # Other great providers exist like Cerebras, ElevenLabs, Groq, Play.ht, Rime, and more
+        # Learn more and pick the best one for your app:
+        # https://docs.livekit.io/agents/plugins
+        super().__init__(
+            instructions="You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+            "You were created as a demo to showcase the capabilities of LiveKit's agents framework.",
+            stt=deepgram.STT(),
+            llm=openai.LLM(model="gpt-4o-mini"),
+            tts=cartesia.TTS(),
+            # use LiveKit's transformer-based turn detector
+            turn_detection=MultilingualModel(),
+        )
+
+    async def on_enter(self):
+        # The agent should be polite and greet the user when it joins :)
+        self.session.generate_reply(
+            instructions="Hey, how can I help you today?", allow_interruptions=True
+        )
 
 
 def prewarm(proc: JobProcess):
@@ -30,15 +55,6 @@ def prewarm(proc: JobProcess):
 
 
 async def entrypoint(ctx: JobContext):
-    initial_ctx = llm.ChatContext().append(
-        role="system",
-        text=(
-            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
-            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
-            "You were created as a demo to showcase the capabilities of LiveKit's agents framework."
-        ),
-    )
-
     logger.info(f"connecting to room {ctx.room.name}")
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
 
@@ -46,38 +62,33 @@ async def entrypoint(ctx: JobContext):
     participant = await ctx.wait_for_participant()
     logger.info(f"starting voice assistant for participant {participant.identity}")
 
-    # This project is configured to use Deepgram STT, OpenAI LLM and Cartesia TTS plugins
-    # Other great providers exist like Cerebras, ElevenLabs, Groq, Play.ht, Rime, and more
-    # Learn more and pick the best one for your app:
-    # https://docs.livekit.io/agents/plugins
-    agent = VoicePipelineAgent(
-        vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(),
-        llm=openai.LLM(model="gpt-4o-mini"),
-        tts=cartesia.TTS(),
-        # use LiveKit's transformer-based turn detector
-        turn_detector=turn_detector.EOUModel(),
-        # minimum delay for endpointing, used when turn detector believes the user is done with their turn
-        min_endpointing_delay=0.5,
-        # maximum delay for endpointing, used when turn detector does not believe the user is done with their turn
-        max_endpointing_delay=5.0,
-        # enable background voice & noise cancellation, powered by Krisp
-        # included at no additional cost with LiveKit Cloud
-        noise_cancellation=noise_cancellation.BVC(),
-        chat_ctx=initial_ctx,
-    )
-
     usage_collector = metrics.UsageCollector()
 
-    @agent.on("metrics_collected")
+    # Log metrics and collect usage data
     def on_metrics_collected(agent_metrics: metrics.AgentMetrics):
         metrics.log_metrics(agent_metrics)
         usage_collector.collect(agent_metrics)
 
-    agent.start(ctx.room, participant)
+    session = AgentSession(
+        vad=ctx.proc.userdata["vad"],
+        # minimum delay for endpointing, used when turn detector believes the user is done with their turn
+        min_endpointing_delay=0.5,
+        # maximum delay for endpointing, used when turn detector does not believe the user is done with their turn
+        max_endpointing_delay=5.0,
+    )
 
-    # The agent should be polite and greet the user when it joins :)
-    await agent.say("Hey, how can I help you today?", allow_interruptions=True)
+    # Trigger the on_metrics_collected function when metrics are collected
+    session.on("metrics_collected", on_metrics_collected)
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # enable background voice & noise cancellation, powered by Krisp
+            # included at no additional cost with LiveKit Cloud
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
 
 
 if __name__ == "__main__":
